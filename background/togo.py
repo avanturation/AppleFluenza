9bf45b1e58fc3ec96f8897d685ec53f0df5ae4aa
HyunCha <truetype24@gmail.com>
2021-01-19 11:26:38 +0900

    Update togo.py

--- background/togo.py
+++ background/togo.py
@@ -9,6 +9,14 @@ from discord.ext import commands
 TOGOURL = "https://samsung.partnerbot.cloudturing.chat/api/togo/stocks/onsite"
 CHANNELID = 800755427200466964
 
+def compare(a, b):
+    if a["S21"] < b["S21"]:
+        return True
+    elif a["S21+"] < b["S21+"]:
+        return True
+    elif a["S21Ultra"] < b["S21Ultra"]:
+        return True
+
 async def GetData():
     data = await Parser.request(TOGOURL)
     data = orjson.loads(data)
@@ -22,16 +30,25 @@ async def CheckGalaxyToGo(bot: commands.Bot) -> Any:
         now = await GetData()
         if prev != now:
             for a, b in zip(prev, now):
-                if str(a) != str(b) and a["location"] == "경기":
-                    embed = discord.Embed (
-                        title=f"알림! {a['name']}점 입고"
-                    )
-                    embed.add_field (
-                        name="현재 수량",
-                        value=f"S21: {b['S21']}대, S21+: {b['S21+']}대, S21 Ultra: {b['S21Ultra']}대"
-                    )
-                    await channel.send(embed=embed)
-                    await channel.send("<@!537227583729958924> <@!295371171979853825>")
+                if str(a) != str(b)
+                    if a["location"] == "경기":
+                        if compare(a, b):
+                            q, w, e = ""
+                            embed = discord.Embed (
+                                title=f"알림! {a['name']}점 입고"
+                            )
+                            if a["S21"] < b["S21"]:
+                                q = f"S21: {b['S21']}대, "
+                            if a["S21+"] < b["S21+"]:
+                                w = f"S21+: {b['S21+']}대, "
+                            if a["S21Ultra"] < b["S21Ultra"]:
+                                e = f"S21 Ultra: {b['S21Ultra']}대"
+                            embed.add_field (
+                                name="현재 수량",
+                                value=f"{q}{w}{e}"
+                            )
+                            await channel.send(embed=embed)
+                            await channel.send("<@!537227583729958924> <@!295371171979853825>")
         prev = now
                     
 
