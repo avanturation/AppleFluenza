c25f7cd4e961c9f9ab154afc072d23044fe6220d
fxrcha <truetype24@gmail.com>
2021-02-15 21:49:05 +0900

    chore(bot): added logger

--- Siri/bot.py
+++ Siri/bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from .utils.getenv import getenv
+from .utils.logger import Log
 
 
 class Siri(commands.Bot):
@@ -13,7 +14,9 @@ class Siri(commands.Bot):
             intents=discord.Intents.all(),
         )
         self.dbkr_token = getenv("KOREANBOTS_TOKEN")
-        self.uqbts_token = getenv("UNIQUEBOTS_TOKEN")
+        self.uqbots_token = getenv("UNIQUEBOTS_TOKEN")
+        self.logger = Log.defaultLogger("Siri")
+        self.discord_logger = Log.discordLogger()
 
 
 def auto_load_cogs(bot: Siri):
@@ -21,14 +24,14 @@ def auto_load_cogs(bot: Siri):
 
     for i in cmdlist:
         if i.endswith(".py") and not i.startswith("__"):
-
             cmdname = f"Siri.cogs.{i.replace('.py', '')}"
 
             try:
                 bot.load_extension(cmdname)
+                bot.logger.log(f"{cmdname} Cog successfully loaded.")
 
             except Exception as error:
-                print(f"{cmdname} failed to load: {error}")
+                bot.logger.log(f"{cmdname} failed to load: {error}")
 
 
 bot = Siri()
---
+++ Siri/utils/logger.py
@@ -0,0 +1,54 @@
+import logging
+
+FORMATTER = logging.Formatter("[%(levelname)s][%(name)s][%(asctime)s]: %(message)s")
+
+
+class Log:
+    @staticmethod
+    def cogLogger(cog) -> logging.Logger:
+        name = cog.__class__.__qualname__
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+
+        if not logger.hasHandlers():
+            streamhandler = logging.StreamHandler()
+            streamhandler.setFormatter(FORMATTER)
+            filehandler = logging.FileHandler(f"Siri/logs/{name}.txt", "a")
+            filehandler.setFormatter(FORMATTER)
+            logger.addHandler(streamhandler)
+            logger.addHandler(filehandler)
+
+        logger.info(f"{name} Loaded.")
+        return logger
+
+    @staticmethod
+    def defaultLogger(name) -> logging.Logger:
+        log = logging.getLogger(name)
+        log.setLevel(logging.INFO)
+
+        if not log.hasHandlers():
+            stream_handler = logging.StreamHandler()
+            filehandler = logging.FileHandler("Siri/logs/{}.txt".format(name), "a")
+            stream_handler.setFormatter(FORMATTER)
+            filehandler.setFormatter(FORMATTER)
+            log.addHandler(filehandler)
+            log.addHandler(stream_handler)
+
+        log.info(f"{name} Loaded.")
+        return log
+
+    @staticmethod
+    def discordLogger() -> logging.Logger:
+        logger = logging.getLogger("discord")
+        logger.setLevel(logging.INFO)
+
+        if not logger.hasHandlers():
+            streamhandler = logging.StreamHandler()
+            streamhandler.setFormatter(FORMATTER)
+            filehandler = logging.FileHandler(f"Siri/logs/discord.txt", "a")
+            filehandler.setFormatter(FORMATTER)
+            logger.addHandler(streamhandler)
+            logger.addHandler(filehandler)
+
+        logger.info(f"Discord Loaded.")
+        return logger
