62c285add7ff0b2348a041c698bb053da4082c9d
fxrcha <truetype24@gmail.com>
2021-02-22 18:55:25 +0900

    chore(multilang): caching

--- Siri/utils/multilang.py
+++ Siri/utils/multilang.py
@@ -1,15 +1,28 @@
 from discord import Guild, VoiceRegion
+from ..bot import bot
 import json
+
+cache = {}
 
 
 def discrim_region(object: Guild) -> str:
-    if object.region == VoiceRegion.south_korea:
-        return "kr"
-    else:
-        return "en"
+    return "kr" if object.region == VoiceRegion.south_korea else "en"
 
 
 def load_lang_pack(region: str, cog_name: str, target: str) -> str:
     path = f"Siri/langpack/{cog_name}_{region}.json"
-    lang_pack = json.loads(path)
+
+    if cache[path]:
+        return cache[path][target]
+
+    try:
+        lang_pack = json.loads(path)
+    except json.decoder.JSONDecodeError:
+        bot.logger.warn(
+            f"WARN: Loading langpack for language '{region}' has failed. Using default 'en'"
+        )
+        lang_pack = json.loads(f"Siri/langpack/{cog_name}_en.json")
+
+    cache[path] = lang_pack
+
     return lang_pack[target]
