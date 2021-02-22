0dbf56407c566c23ad6fea6d24c01efb0b3d5b53
fxrcha <truetype24@gmail.com>
2021-02-22 21:38:37 +0900

    fix(ipsw): PyApple client

--- Siri/cogs/ipsw.py
+++ Siri/cogs/ipsw.py
@@ -1,4 +1,3 @@
-from discord import client
 import pyapple
 import discord
 from discord.ext import commands
@@ -11,13 +10,12 @@ class IPSWCogs(commands.Cog):
 
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.logger
         self.client = pyapple.Client()
 
     @commands.command(name="기기정보", aliases=["device"])
     async def device_command(self, ctx, identfier: str):
         guild_region = discrim_region(ctx.guild)
-        device_info = await client.device(identfier)
+        device_info = await self.client.device(identfier)
 
         embed = discord.Embed(title="")
 
